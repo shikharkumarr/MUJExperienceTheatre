01c0beeea2842c540c07e4e9a1c726e341f573a4
sehaj-79 <bhasin.seh79@gmail.com>
2023-02-20 15:20:14 +0530

    SignInActivity.java : Activity updated - user added on realtime database

--- app/src/main/java/com/aliferous/mujtheatrebooking/ViewTicketsActivity.java
+++ app/src/main/java/com/aliferous/mujtheatrebooking/ViewTicketsActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class ViewTicketsActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
