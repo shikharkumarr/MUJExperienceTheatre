a48c4add607a36106f6a14209c7439be52556830
Shikhar Kumar <shikharkumar009@gmail.com>
2023-02-21 00:07:22 +0530

    MainActivity btn updated

--- app/src/main/java/com/aliferous/mujtheatrebooking/MainActivity.java
+++ app/src/main/java/com/aliferous/mujtheatrebooking/MainActivity.java
@@ -9,16 +9,25 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
+    Button button1, button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ViewTicketsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ChooseSessionActivity.class);
