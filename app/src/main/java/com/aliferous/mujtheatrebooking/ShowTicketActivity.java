a2fa2030fd4b5da9920b64211f216a3d2ada00f0
Shikhar Kumar <shikharkumar009@gmail.com>
2023-02-24 15:48:54 +0530

    QR updated

--- app/src/main/java/com/aliferous/mujtheatrebooking/ShowTicketActivity.java
+++ app/src/main/java/com/aliferous/mujtheatrebooking/ShowTicketActivity.java
@@ -2,19 +2,32 @@ package com.aliferous.mujtheatrebooking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class ShowTicketActivity extends AppCompatActivity {
 
     TextView tvBack, tvBID, tvName, tvEmail, tvDateTime;
+    ImageView imQR;
+    String myText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_ticket);
+
 
         Intent mIntent = getIntent();
         int BookingID = mIntent.getIntExtra("BookingID",0);
@@ -30,12 +43,28 @@ public class ShowTicketActivity extends AppCompatActivity {
         tvName = findViewById(R.id.tvName);
         tvEmail = findViewById(R.id.tvEmail);
         tvDateTime = findViewById(R.id.tvDatetime);
+        imQR = findViewById(R.id.imageView5);
 
 
         tvBID.setText("Booking ID : "+BookingID);
         tvName.setText(name);
         tvEmail.setText(email);
         tvDateTime.setText(Date +"   "+ Time);
+
+        myText = ""+BookingID;
+
+        MultiFormatWriter mWriter = new MultiFormatWriter();
+        try {
+            //BitMatrix class to encode entered text and set Width & Height
+            BitMatrix mMatrix = mWriter.encode(myText, BarcodeFormat.QR_CODE, 400,400);
+            BarcodeEncoder mEncoder = new BarcodeEncoder();
+            Bitmap mBitmap = mEncoder.createBitmap(mMatrix);//creating bitmap of code
+            imQR.setImageBitmap(mBitmap);//Setting generated QR code to imageView
+            // to hide the keyboard
+            InputMethodManager manager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
 
         tvBack.setOnClickListener(new View.OnClickListener() {
             @Override
