50c4e8814a47f36c69ecf6c55dc0d7d9b95ac287
sehaj-79 <bhasin.seh79@gmail.com>
2023-02-24 15:16:01 +0530

    BookingId_Model.java : Model Created

---
+++ app/src/main/java/Model/BookingId_Model.java
@@ -0,0 +1,21 @@
+package Model;
+
+public class BookingId_Model {
+
+    private String Id;
+
+    public BookingId_Model(String Id){
+        this.Id = Id;
+    }
+
+    public BookingId_Model(){}
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+}
